6cf472a6c746aa6a2e7adced7be643a3687338a4
Darwin Quiñones <darwinquinones735@gmail.com>
2024-02-11 11:30:08 -0500

    Finish product display

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import Breadcrum from '../Components/Breadcrums/Breadcrum.jsx';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay.jsx';
 import { ShopContext } from '../Context/ShopContext';
 
 const Product = () => {
@@ -13,6 +14,7 @@ const Product = () => {
     return (
         <div>
             <Breadcrum product={ product } />
+            <ProductDisplay product={ product } />
         </div>
     );
 }
