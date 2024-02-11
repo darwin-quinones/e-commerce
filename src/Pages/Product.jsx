54b52c4ed4286f2def65de2ea8871f78e3f2b0ea
Darwin Quiñones <darwinquinones735@gmail.com>
2024-02-11 10:11:47 -0500

    Add a breadcrum of the product

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum.jsx';
+import { ShopContext } from '../Context/ShopContext';
 
 const Product = () => {
+
+    const { all_product } = useContext(ShopContext)
+    const { productId } = useParams()
+    // retrieve a product
+    const product = all_product.find((e) => e.id === Number(productId))
+
     return (
         <div>
-            
+            <Breadcrum product={ product } />
         </div>
     );
 }
